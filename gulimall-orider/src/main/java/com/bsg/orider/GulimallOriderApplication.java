2167a5f080ad389cd481c3a07a1ec0a3e505eba4
xxiaolixiang <xxiao.lixiang@contract.isid.co.jp>
2022-05-31 09:53:20 +0800

    初始创建

---
+++ gulimall-orider/src/main/java/com/bsg/orider/GulimallOriderApplication.java
@@ -0,0 +1,13 @@
+package com.bsg.orider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOriderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallOriderApplication.class, args);
+    }
+
+}
---
+++ gulimall-orider/src/test/java/com/bsg/orider/GulimallOriderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsg.orider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOriderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
